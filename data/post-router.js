d5cd43e4eccf3cfefef297c0fd825ee2c8ed44b3
Areum Jo <areumjo1@gmail.com>
2019-10-05 18:29:11 -0700

    MVP completed: all endpoints work well

--- data/post-router.js
+++ data/post-router.js
@@ -112,4 +112,48 @@ router.post('/:id/comments', (req, res) => {
         })
 });
 
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    Posts.remove(id)
+        .then(deledted => {
+            if (deledted) {
+                res.status(200).json(deledted);
+            } else {
+                res.status(400).json({
+                    message: "The post with the specified ID does not exist."
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: "The post could not be removed"
+            })
+        })
+});
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const updated = req.body;
+    if (!req.body.title || !req.body.contents) {
+        res.status(400).json({
+            errorMessage: "Please provide title and contents for the post."
+        })
+    }
+    Posts.update(id, updated)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post)
+            } else {
+                res.status(404).json({
+                    message: "The post with the specified ID does not exist."
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: "The post information could not be modified."
+            })
+        })
+})
+
 module.exports = router;
